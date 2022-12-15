c85884e20c88efdee826fbc0896daf76605491f1
paguerre3 <pablo.paguerre@mercadolibre.com>
2022-12-14 23:37:50 -0300

    ddd organization

--- src/main/java/com/capitol/pricing/PricingApplication.java
+++ src/main/java/com/capitol/pricing/PricingApplication.java
@@ -1,4 +1,4 @@
-package com.capitol.pricing;
+package com.capitol.pricing.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/capitol/pricing/PricingApplicationTests.java
+++ src/test/java/com/capitol/pricing/PricingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.capitol.pricing;
+package com.capitol.pricing.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
